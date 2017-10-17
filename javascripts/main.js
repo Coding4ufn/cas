972769812fbc7b5aab2c74e78e1cb1b44930c6aa
Misagh Moayyed <mmoayyed@unicon.net>
2017-10-17 13:14:54 +0330

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -334,13 +334,7 @@ function generateDependencyLangFragments() {
           <a class='dropdown-item' href='https://github.com/apereo/cas-gradle-overlay-template'>CAS Server Gradle Overlay</a> \
           <div class='dropdown-divider'></div> \
           <a class='dropdown-item' href='https://github.com/apereo/cas-services-management-overlay'>CAS Management Maven Overlay</a> \
-          <a class='dropdown-item' href='https://github.com/apereo/cas-services-management-gradle-overlay'>CAS Management Gradle Overlay</a> \ 
-	  <div class='dropdown-divider'></div> \
-	  <a class='dropdown-item' href='https://github.com/apereo/cas-configserver-overlay'>CAS Configuration Server Overlay</a> \
-	  <a class='dropdown-item' href='https://github.com/apereo/cas-discoveryserver-overlay'>CAS Discovery Server Overlay</a> \
-	  <a class='dropdown-item' href='https://github.com/apereo/cas-bootadmin-overlay'>CAS Spring Boot Admin Server</a> \	
-	  <div class='dropdown-divider'></div> \
-          <a class='dropdown-item' href='https://github.com/apereo/cas-webapp-docker'>Dockerized CAS Deployment</a> \
+          <a class='dropdown-item' href='https://github.com/apereo/cas-services-management-gradle-overlay'>CAS Management Gradle Overlay</a> \
       </div> \
   </li> \
   </ul> \
